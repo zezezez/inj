29a914174e56a41661c4b720cdecb32d1f5780fc
zezezez <52458706+zezezez@users.noreply.github.com>
2023-12-21 15:34:27 +0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import AboutUs from "@/components/AboutUs";
+import Minter from "@/components/Minter";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center space-x-48 p-24">
+      <AboutUs />
+      <Minter />
+    </main>
+  );
+}
